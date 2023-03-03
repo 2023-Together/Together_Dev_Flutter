171d122694b8d9a6cd93cae323d7839b1fed4859
uomaep <uomaep@gmail.com>
2023-03-04 01:31:12 +0900

    feat: add my schedule page
    bottom navigation bar 각각의 메뉴를 누르면 해당 페이지로 이동할 수
    있도록 함 추후 개선 필요

--- lib/features/main/widgets/main_navbar.dart
+++ lib/features/main/widgets/main_navbar.dart
@@ -47,12 +47,15 @@ class _MainNavbarState extends State<MainNavbar> {
         BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
         BottomNavigationBarItem(icon: Icon(Icons.chat_rounded), label: '단톡방'),
         BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today_outlined), label: '내 일정'),
+          icon: Icon(Icons.calendar_today_outlined),
+          label: '내 일정',
+        ),
         BottomNavigationBarItem(
             icon: Icon(Icons.account_tree_outlined), label: '팀원모집'),
         BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle_outlined), label: '내정보',),
-        
+          icon: Icon(Icons.account_circle_outlined),
+          label: '내정보',
+        ),
       ],
     );
   }
