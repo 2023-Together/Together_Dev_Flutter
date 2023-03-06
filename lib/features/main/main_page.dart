c21cac9940e4c66c2e41183dda0b93792584b88f
wogus1027 <dlwogus1027@naver.com>
2023-03-06 16:52:15 +0900

    메인 페이지 네비게이션 겹친 부분 수정

--- lib/features/main/main_page.dart
+++ lib/features/main/main_page.dart
@@ -4,19 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:swag_cross_app/constants/gaps.dart';
 import 'package:swag_cross_app/constants/sizes.dart';
+import 'package:swag_cross_app/features/main/widgets/main_alert.dart';
 import 'package:swag_cross_app/features/mainNavigation/mian_navigation.dart';
 import 'package:swag_cross_app/features/main/widgets/main_button.dart';
 import 'package:swag_cross_app/features/sign_in_up/sign_in_main.dart';
 import 'package:swag_cross_app/features/storages/secure_storage_login.dart';
 
-import '../schedule/screen.dart';
-
 class MainPage extends StatefulWidget {
-  final int HOME_PAGE = 0;
-  final int CHAT_PAGE = 1;
-  final int SCHEDULE_PAGE = 2;
-  final int TEAM_PAGE = 3;
-  final int MY_PAGE = 4;
+  // final int HOME_PAGE = 0;
+  // final int CHAT_PAGE = 1;
+  // final int SCHEDULE_PAGE = 2;
+  // final int TEAM_PAGE = 3;
+  // final int MY_PAGE = 4;
 
   const MainPage({super.key});
 
@@ -50,7 +49,7 @@ class _MainPageState extends State<MainPage>
     super.initState();
 
     var loginType = SecureStorageLogin.getLoginType();
-    if (loginType != "none" || loginType != null) {
+    if (loginType != "none") {
       _isLogined = true;
     }
   }
@@ -125,132 +124,132 @@ class _MainPageState extends State<MainPage>
     );
   }
 
-  void _onTap(int index) {
-    setState(() {
-      switch (index) {
-        case 0:
-          _currentPage = widget.HOME_PAGE;
-          break;
-        case 1:
-          _currentPage = widget.CHAT_PAGE;
-          break;
-        case 2:
-          _currentPage = widget.SCHEDULE_PAGE;
-          break;
-        case 3:
-          _currentPage = widget.TEAM_PAGE;
-          break;
-        case 4:
-          _currentPage = widget.MY_PAGE;
-          break;
-      }
-    });
-    print(_currentPage);
-  }
+  // void _onTap(int index) {
+  //   setState(() {
+  //     switch (index) {
+  //       case 0:
+  //         _currentPage = widget.HOME_PAGE;
+  //         break;
+  //       case 1:
+  //         _currentPage = widget.CHAT_PAGE;
+  //         break;
+  //       case 2:
+  //         _currentPage = widget.SCHEDULE_PAGE;
+  //         break;
+  //       case 3:
+  //         _currentPage = widget.TEAM_PAGE;
+  //         break;
+  //       case 4:
+  //         _currentPage = widget.MY_PAGE;
+  //         break;
+  //     }
+  //   });
+  //   print(_currentPage);
+  // }
 
-  BottomNavigationBar _bottomNavigationBar() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: Colors.white,
-      selectedItemColor: const Color.fromARGB(255, 92, 27, 243),
-      unselectedItemColor: Colors.black,
-      selectedFontSize: 16,
-      unselectedFontSize: 16,
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      currentIndex: _currentPage,
-      onTap: _onTap,
-      items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-        BottomNavigationBarItem(icon: Icon(Icons.chat_rounded), label: '단톡방'),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today_outlined),
-          label: '내 일정',
-        ),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.account_tree_outlined), label: '팀원모집'),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.account_circle_outlined),
-          label: '내정보',
-        ),
-      ],
-    );
-  }
+  // BottomNavigationBar _bottomNavigationBar() {
+  //   return BottomNavigationBar(
+  //     type: BottomNavigationBarType.fixed,
+  //     backgroundColor: Colors.white,
+  //     selectedItemColor: const Color.fromARGB(255, 92, 27, 243),
+  //     unselectedItemColor: Colors.black,
+  //     selectedFontSize: 16,
+  //     unselectedFontSize: 16,
+  //     showSelectedLabels: true,
+  //     showUnselectedLabels: true,
+  //     currentIndex: _currentPage,
+  //     onTap: _onTap,
+  //     items: const [
+  //       BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+  //       BottomNavigationBarItem(icon: Icon(Icons.chat_rounded), label: '단톡방'),
+  //       BottomNavigationBarItem(
+  //         icon: Icon(Icons.calendar_today_outlined),
+  //         label: '내 일정',
+  //       ),
+  //       BottomNavigationBarItem(
+  //           icon: Icon(Icons.account_tree_outlined), label: '팀원모집'),
+  //       BottomNavigationBarItem(
+  //         icon: Icon(Icons.account_circle_outlined),
+  //         label: '내정보',
+  //       ),
+  //     ],
+  //   );
+  // }
 
-  Widget _body() {
-    switch (_currentPage) {
-      case 0:
-        return SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: Sizes.size20,
-              horizontal: Sizes.size32,
-            ),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 110,
-                    color: Colors.blue,
-                    child: const Center(
-                      child: Text(
-                        "배너",
-                        style: TextStyle(
-                          fontSize: Sizes.size20,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Gaps.v16,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: _onVolSearchTap,
-                        child: const MainButton(text: "봉사 찾기"),
-                      ),
-                      GestureDetector(
-                        onTap: _onCommunityTap,
-                        child: const MainButton(text: "공지사항"),
-                      ),
-                    ],
-                  ),
-                  Gaps.v20,
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: _onTest1Tap,
-                        child: const MainButton(text: "로그인 테스트1"),
-                      ),
-                      GestureDetector(
-                        onTap: _onTest2Tap,
-                        child: const MainButton(text: "로그인 테스트2"),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      case 1:
-        return Container();
-      case 2:
-        return const TableCalendarPage();
-      case 3:
-        return Container();
-      case 4:
-        return Container();
-      default:
-        return Container();
-    }
-  }
+  // Widget _body() {
+  //   switch (_currentPage) {
+  //     case 0:
+  //       return SafeArea(
+  //         child: Padding(
+  //           padding: const EdgeInsets.symmetric(
+  //             vertical: Sizes.size20,
+  //             horizontal: Sizes.size32,
+  //           ),
+  //           child: Center(
+  //             child: Column(
+  //               mainAxisAlignment: MainAxisAlignment.start,
+  //               children: [
+  //                 Container(
+  //                   height: 110,
+  //                   color: Colors.blue,
+  //                   child: const Center(
+  //                     child: Text(
+  //                       "배너",
+  //                       style: TextStyle(
+  //                         fontSize: Sizes.size20,
+  //                       ),
+  //                     ),
+  //                   ),
+  //                 ),
+  //                 Gaps.v16,
+  //                 Row(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                   children: [
+  //                     GestureDetector(
+  //                       onTap: _onVolSearchTap,
+  //                       child: const MainButton(text: "봉사 찾기"),
+  //                     ),
+  //                     GestureDetector(
+  //                       onTap: _onCommunityTap,
+  //                       child: const MainButton(text: "공지사항"),
+  //                     ),
+  //                   ],
+  //                 ),
+  //                 Gaps.v20,
+  //                 Row(
+  //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  //                   children: [
+  //                     GestureDetector(
+  //                       onTap: _onTest1Tap,
+  //                       child: const MainButton(text: "로그인 테스트1"),
+  //                     ),
+  //                     GestureDetector(
+  //                       onTap: _onTest2Tap,
+  //                       child: const MainButton(text: "로그인 테스트2"),
+  //                     ),
+  //                   ],
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //         ),
+  //       );
+  //     case 1:
+  //       return Container();
+  //     case 2:
+  //       return const TableCalendarPage();
+  //     case 3:
+  //       return Container();
+  //     case 4:
+  //       return Container();
+  //     default:
+  //       return Container();
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    // final size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
         children: [
@@ -367,63 +366,7 @@ class _MainPageState extends State<MainPage>
             ),
           SlideTransition(
             position: _alertAnimation,
-            child: SafeArea(
-              child: Container(
-                height: 500,
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size18,
-                  horizontal: Sizes.size10,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(Sizes.size8),
-                    bottomRight: Radius.circular(Sizes.size8),
-                  ),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          "알림창",
-                          style: TextStyle(
-                            fontSize: Sizes.size20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
-                      ],
-                    ),
-                    Gaps.v18,
-                    const Divider(thickness: 1, height: 1, color: Colors.black),
-                    Gaps.v10,
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(Sizes.size14),
-                      ),
-                      child: const ListTile(
-                        title: Text("알림1"),
-                        subtitle: Text("내용"),
-                      ),
-                    ),
-                    Gaps.v5,
-                    Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(Sizes.size14),
-                      ),
-                      child: const ListTile(
-                        title: Text("알림1"),
-                        subtitle: Text("내용"),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+            child: const MainAlert(),
           ),
         ],
       ),
