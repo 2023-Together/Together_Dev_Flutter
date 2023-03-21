cd621c63ab70331eedcbdd1b4aea8cec02506cce
wogus1027 <dlwogus1027@naver.com>
2023-03-21 17:47:50 +0900

    주석 추가

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -1,5 +1,14 @@
 import 'dart:io';
 
+// 구글 라이브러리 Key
+// App ID : ca-app-pub-8792702490232026~8158571289
+// 	- 배너 : ca-app-pub-8792702490232026/5841483410
+// 	- 네이티브 고급 : ca-app-pub-8792702490232026/3602332882
+
+// Android Id : ca-app-pub-8792702490232026~4276248161
+// 	- 배너 : ca-app-pub-8792702490232026/1030658792
+// 	- 네이티브 고급 : ca-app-pub-8792702490232026/5244633336
+
 class AdHelper {
   // 테스트용 id
   static String get bannerAdUnitId {
