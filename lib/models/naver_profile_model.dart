59db8ed974e3c16995d94ab3f51bd675ca754f11
이재현 <dlwogus1027@naver.com>
2023-07-16 20:09:53 +0900

    네이버프로필모델 수정

--- lib/models/naver_profile_model.dart
+++ lib/models/naver_profile_model.dart
@@ -1,55 +1,28 @@
 class NaverProfileModel {
-  // String nickname;
-  // String id;
-  // String name;
-  // String email;
-  // String gender;
-  // String age;
-  // String birthday;
-  // String birthyear;
-  // String profileImage;
-  // String mobile;
-
-  String userAcid; // 로그인 id
-  String userPw; // 로그인 비밀번호
-  String userName; // 유저 실명
-  String userProfileImage; // 프로필 이미지
-  String userDef; // 설명
-  String userType; // 로그인 타입
-  Map<String, dynamic> userSns; // json 데이터
-
-  // NaverProfileModel({
-  //   required this.nickname,
-  //   required this.id,
-  //   required this.name,
-  //   required this.email,
-  //   required this.gender,
-  //   required this.age,
-  //   required this.birthday,
-  //   required this.birthyear,
-  //   required this.profileImage,
-  //   required this.mobile,
-  // });
+  String id;
+  String name;
+  String email;
+  String gender;
+  String birthday;
+  String birthyear;
+  String mobile;
 
   NaverProfileModel({
-    required this.userAcid,
-    required this.userPw,
-    required this.userName,
-    required this.userProfileImage,
-    required this.userDef,
-    required this.userType,
-    required this.userSns,
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.gender,
+    required this.birthday,
+    required this.birthyear,
+    required this.mobile,
   });
 
-  // NaverProfileModel.fromJson(Map<String, dynamic> json)
-  //     : userAcid = json['userAcid'],
-  //       userPw = json['userPw'],
-  //       userName = json['name'],
-  //       email = json['email'],
-  //       gender = json['gender'],
-  //       age = json['age'],
-  //       birthday = json['birthday'],
-  //       birthyear = json['birthyear'],
-  //       userProfileImage = json['profileImage'],
-  //       userSns = json;
+  NaverProfileModel.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        email = json['email'],
+        gender = json['gender'],
+        birthday = json['birthday'],
+        birthyear = json['birthyear'],
+        mobile = json['mobile'];
 }
